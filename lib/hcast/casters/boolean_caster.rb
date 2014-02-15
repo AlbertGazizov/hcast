204db33226bfc83f78c666d055a3769177db8696
Albert Gazizov <deeper4k@gmail.com>
2014-02-15 21:39:29 +0400

    Add value to boolean caster

--- lib/hcast/casters/boolean_caster.rb
+++ lib/hcast/casters/boolean_caster.rb
@@ -3,9 +3,9 @@ class HCast::Casters::BooleanCaster
   def self.cast(value, attr_name, options = {})
     if [TrueClass, FalseClass].include?(value.class)
       value
-    elsif ['1', 'true', 1].include?(value)
+    elsif ['1', 'true', 'on', 1].include?(value)
       true
-    elsif ['0', 'false', 0].include?(value)
+    elsif ['0', 'false', 'off', 0].include?(value)
       false
     else
       raise HCast::Errors::CastingError, "#{attr_name} should be a boolean"
