356fc4bd0a65477d0013fecf2a4a8577e0155188
Roman Heinrich <roman.heinrich@gmail.com>
2016-12-31 15:23:41 +0100

    simplify conditional logic in hash_caster

--- lib/hcast/casters/hash_caster.rb
+++ lib/hcast/casters/hash_caster.rb
@@ -1,11 +1,8 @@
 class HCast::Casters::HashCaster
 
   def self.cast(value, attr_name, options = {})
-    if value.is_a?(Hash)
-      value
-    else
-      raise HCast::Errors::CastingError, "should be a hash"
-    end
+    return value if value.is_a?(Hash)
+    raise HCast::Errors::CastingError, "should be a hash"
   end
 
 end
