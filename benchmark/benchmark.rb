4b403917382adc9ade7117350f244251501dd846
Roman Heinrich <roman.heinrich@gmail.com>
2017-01-01 19:26:16 +0100

    change benchmark to be started from a static method

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -7,50 +7,60 @@ require 'bixby/bench'
 
 require_relative "casters"
 
-module CastingBenchmark
-  input_contact = {
-    contact: {
-      name: "John Smith",
-      age: "22",
-      weight: "65.5",
-      birthday: "2014-02-02",
-      last_logged_in: "2014-02-02 10:10:00",
-      last_visited_at: "2014-02-02 10:10:00",
-      company: {
-        name: "MyCo",
+class CastingBenchmark
+  def input_contact
+   @input_contact ||= {
+      contact: {
+        name: "John Smith",
+        age: "22",
+        weight: "65.5",
+        birthday: "2014-02-02",
+        last_logged_in: "2014-02-02 10:10:00",
+        last_visited_at: "2014-02-02 10:10:00",
+        company: {
+          name: "MyCo",
+        },
+        emails: [ "test@example.com", "test2@example.com" ],
+        social_accounts: [
+          {
+            name: "john_smith",
+            type: 'twitter',
+          },
+          {
+            name: "John",
+            type: :facebook,
+          },
+        ]
+      }
+    }
+  end
+
+  def input_company
+    @input_company ||= {
+      name: 'Might & Magic',
+      settings: {
+        account: :'migthy_lord'
       },
-      emails: [ "test@example.com", "test2@example.com" ],
-      social_accounts: [
-        {
-          name: "john_smith",
-          type: 'twitter',
-        },
-        {
-          name: "John",
-          type: :facebook,
-        },
+      emails: [
+        { address: :'test1@example.com' },
+        { address: :'test2@example.com' },
       ]
     }
-  }
+  end
 
-  input_company = {
-    name: 'Might & Magic',
-    settings: {
-      account: :'migthy_lord'
-    },
-    emails: [
-      { address: :'test1@example.com' },
-      { address: :'test2@example.com' },
-    ]
-  }
+  def self.run
+    instance = CastingBenchmark.new
+    Bixby::Bench.run(10_000) do |b|
+      b.sample('Contact Caster') do
+        ContactCaster.cast(instance.input_contact)
+      end
 
-  Bixby::Bench.run(10_000) do |b|
-    b.sample('Contact Caster') do
-      ContactCaster.cast(input_contact)
-    end
-
-    b.sample("CompanyCaster") do
-      CompanyCaster.cast(input_company)
+      b.sample("CompanyCaster") do
+        CompanyCaster.cast(instance.input_company)
+      end
     end
   end
 end
+
+CastingBenchmark.run
+
